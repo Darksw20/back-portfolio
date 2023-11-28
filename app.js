604c9f0159a84fc431d7547a840af538658503d2
scout <scout.dks@gmail.com>
2023-11-27 21:06:41 -0600

    Skill endpoint working

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors({ origin: process.env.FRONTEND }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/', indexRouter);
+app.use("/api/", indexRouter);
 
 module.exports = app;
