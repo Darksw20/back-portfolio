d8245be5437a1ce0f5f9e12a716abaa7a0e01b8c
Ricardo Navarro <ricardog.navi20@gmail.com>
2024-10-07 15:33:33 -0600

    About example finished

--- api/users.ts
+++ api/users.ts
@@ -5,10 +5,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 	const { user } = req.query as { user?: string };
 	if (req.method === "GET") {
 		try {
-			const result = await sql`SELECT a.*
-				FROM users
-				INNER JOIN achievements a on users.id = a.user_id
-				WHERE users.username=${user};`;
+			const result =
+				await sql`SELECT id,email,username,summary,motivation FROM users WHERE username=${user};`;
 			return res.status(200).json(result.rows);
 		} catch (error) {
 			return res.status(500).json({
