5b2e3e2fceed268c4b5c9a3ae4872b2da1eb3f3a
Ricardo Navarro <ricardog.navi20@gmail.com>
2024-10-10 14:52:01 -0600

    Fixed issues related to dinamic pages

--- api/roles.ts
+++ api/roles.ts
@@ -1,11 +1,22 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { sql } from "@vercel/postgres";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-	const { name = "role" } = req.query;
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+	const { user } = req.query as { user?: string };
 	if (req.method === "GET") {
-		return res.json({
-			message: `GET Hello ${name}!`,
-		});
+		try {
+			const result = await sql`SELECT a.*,r.*
+                FROM users u
+                INNER JOIN public.achievements a on u.id = a.user_id
+                INNER JOIN public.roles_achievements ra on a.id = ra.fk_achievement
+                INNER JOIN public.roles r on r.id = ra.fk_role
+                WHERE u.username=${user};`;
+			return res.status(200).json(result.rows);
+		} catch (error) {
+			return res.status(500).json({
+				message: error,
+			});
+		}
 	} else if (req.method === "POST") {
 		// Process a POST request
 		return res.json({
