85aae91cd3b9a5141064ed3b586058c87f80eb25
Ricardo Navarro <ricardog.navi20@gmail.com>
2024-11-28 07:30:39 -0600

    done

---
+++ api/login.ts
@@ -0,0 +1,34 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { sql } from "@vercel/postgres";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+	const { username, password } = req.query as {
+		username?: string;
+		password?: string;
+	};
+	if (req.method === "GET") {
+		// Process a POST request
+		return res.json({
+			message: `GET Hello ${name}!`,
+		});
+	} else if (req.method === "POST") {
+		try {
+			const result = await sql`
+                SELECT id, email, username, summary, motivation, github, linkedin 
+                FROM users 
+                WHERE username = ${username} and password = ${password};
+            `;
+
+			return res.status(200).json(result.rows.length > 0 ? result.rows[0] : {});
+		} catch (error) {
+			return res.status(500).json({
+				message: error.message || "Internal Server Error",
+			});
+		}
+	} else {
+		// Handle any other HTTP method
+		return res.json({
+			message: `Other Hello ${name}!`,
+		});
+	}
+}
